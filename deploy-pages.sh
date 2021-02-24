4915550cffb09d8ca85474c7817428b52626b53a
Zexi Li <zexi.li@qq.com>
2021-02-24 18:47:29 +0800

    rebuilding helm site Wed Feb 24 06:47:29 PM CST 2021

---
+++ deploy-pages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# copy all generated helm packages to current
+cp -a ./_output/* .
+
+# add changes to git
+git add .
+
+# commit changes
+msg="rebuilding helm site `date`"
+if [ $# -eq 1 ]; then
+    msg="$1"
+fi
+
+git commit -m "$msg"
+
+git push origin gh-pages
