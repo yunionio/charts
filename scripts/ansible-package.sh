bc4c15c0f65576675216377345d543eeaddf276e
Zexi Li <zexi.li@icloud.com>
2022-06-06 17:26:12 +0800

    forvm(nginx): 0.3.2
    Use ansible-telegraf-1.0.0

--- scripts/ansible-package.sh
+++ scripts/ansible-package.sh
@@ -4,6 +4,6 @@ CACHE_DIR="$(pwd)/_cache"
 
 mkdir -p "$CACHE_DIR"
 
-#curl -L https://github.com/yunionio/ansible-telegraf/archive/refs/heads/master.zip -o "$CACHE_DIR"/ansible-telegraf.zip
+curl -L https://github.com/yunionio/ansible-telegraf/archive/1.0.0.tar.gz -o "$CACHE_DIR"/ansible-telegraf-1.0.0.tar.gz
 
 curl -L https://github.com/geerlingguy/ansible-role-nginx/archive/3.1.1.tar.gz -o "$CACHE_DIR"/ansible-role-nginx-3.1.1.tar.gz
